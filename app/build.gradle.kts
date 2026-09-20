242e2c23fa3f1cd4614697f6472637f666729c2a
Nikos Papajohn <nikpapaj@gmail.com>
2026-09-20 04:20:59 +0300

    Mark domain models @Immutable for Compose stability

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.nikpapajohn.moviedb"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.6"
+        versionCode = 7
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
